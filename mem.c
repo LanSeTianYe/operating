98dd4484cb81d085bc97532d533b0f2225064eee
sunfeilong <sunfeilong1993@gmail.com>
2022-03-21 19:56:58 +0800

    update code

--- mem.c
+++ mem.c
@@ -1,28 +1,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "common.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    printf("location of code : %p\n", (void *)main);
 
-    if (argc != 2){
-        fprintf(stderr,"usage: mem <value>\n");
-	exit(1);
-    }
+    printf("location of heap : %p\n", (void *)malloc(1));
 
-    int *intP;
-    intP = malloc(sizeof(int));
-    assert(intP != NULL);
-
-    printf("(%d) memory address of p: %p\n", getpid(), intP);
-    printf("(%d) memory address of &p:%p\n", getpid(), &intP);
-
-    *intP = atoi(argv[1]);
-    while(1){
-        Spin(1);
-	*intP = *intP + 1;
-	printf("(%d) p:%d\n", getpid(), *intP);
-    }
-
-    return 0;
+    int x = 3;
+    printf("location of stack : %p\n", (void *)&x);
+    return x;
 }
